5e4f225090cc099c9d91e72c86824fd0a88444a3
Sean Teague <voodoomon@gmail.com>
2016-04-24 23:48:40 -0700

    Wrangling up the dist directory and cleaning everything up for initial install

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,43 @@
 var path = require('path');
 var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+var plugins = [];
+
+if (process.env.NODE_ENV === 'production') {
+    plugins.push(new webpack.optimize.DedupePlugin());
+    plugins.push(new webpack.optimize.OccurrenceOrderPlugin()),
+    plugins.push(new webpack.optimize.UglifyJsPlugin());
+}
+
+plugins.push(new HtmlWebpackPlugin({
+    title: 'AXIS',
+    filename: 'index.html',
+    template: path.resolve(__dirname, "src/client/template") + '/index.ejs',
+    inject: 'body'
+}));
+
+plugins.push(new CopyWebpackPlugin([
+    {
+        from: path.resolve(__dirname, "src/client/vendor") + '/react-with-addons.min.js',
+        to: "assets/react-with-addons.min.js"
+    }
+]));
 
 module.exports = {
+    devServer: {
+        // This is required for webpack-dev-server. The path should
+        // be an absolute path to your build destination.
+        outputPath: path.join(__dirname, 'dist/client')
+    },
     entry: {
         app: ['./src/client/index.js']
     },
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, "dist/client/assets"),
-        publicPath: 'http://localhost:8090/assets'
+        path: path.resolve(__dirname, "dist/client")//,
+        //publicPath: 'http://localhost:8080/assets'
     },
     module: {
         loaders: [
@@ -22,11 +51,7 @@ module.exports = {
             }
         ]
     },
-    plugins: process.env.NODE_ENV === 'production' ? [
-        new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.OccurrenceOrderPlugin(),
-        new webpack.optimize.UglifyJsPlugin()
-    ] : [],
+    plugins: plugins,
     externals: {
         react: 'React'
     },
